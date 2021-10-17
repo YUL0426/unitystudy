962528b0e1def0dec81a8173e8ceb359c829735f
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-10-17 23:39:24 +0900

    Create PlayerController.cs

---
+++ 5장/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void LbuttonDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+
+    public void RbuttonDown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+}
